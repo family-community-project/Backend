17874705e27907660b940f7ef0c3cb1c51473899
yuncheol-AHN <anchul@hanmail.net>
2025-07-08 00:47:48 +0900

    docs: update swagger-ui (local login)

--- family/src/main/java/com/example/family/user/controller/UserController.java
+++ family/src/main/java/com/example/family/user/controller/UserController.java
@@ -3,7 +3,12 @@ package com.example.family.user.controller;
 import com.example.family.user.dto.request.UserRegisterRequest;
 import com.example.family.user.dto.response.AuthMailResponse;
 import com.example.family.user.dto.response.CodeCheckResponse;
+import com.example.family.user.repository.UserRepository;
 import com.example.family.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,14 +23,21 @@ import java.util.Random;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Tag(name = "Local Login", description = "Local Login API - register, email auth code, etc.")
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     private final JavaMailSender javaMailSender;
     private final Map<String, String> emailAuthStringMap = new HashMap<>();
     private final Map<String, Boolean> emailAuthBooleanMap = new HashMap<>();
 
+    @Operation(summary = "계정 등록 API", description = "신규 계정 등록을 위한 API 입니다")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "register: success or fail"),
+            @ApiResponse(responseCode = "500", description = "internal server error"),
+    })
     @PostMapping("/register")
     public ResponseEntity<Boolean> register(@RequestParam("email") String email,
                            @RequestParam("name") String name,
@@ -40,10 +52,19 @@ public class UserController {
         return responseEntity;
     }
 
+    @Operation(summary = "이메일 인증 코드 전송 API", description = "이메일 인증 코드 전송을 위한 API 입니다. 중복된 이메일이 있을 경우 false를 반환합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "email auth: success(true) or already existed(false)"),
+            @ApiResponse(responseCode = "500", description = "internal server error"),
+    })
     @GetMapping("auth/email")
     public ResponseEntity<AuthMailResponse> authEmail(@RequestParam("email") String email) {
 
-        ResponseEntity<AuthMailResponse> responseEntity = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        if (userRepository.findByEmail(email).isPresent()) {
+            return ResponseEntity.ok(new AuthMailResponse(false, null));
+        }
+
+        ResponseEntity<AuthMailResponse> responseEntity;
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         String authCode = generateAuthString();
         try {
@@ -62,11 +83,17 @@ public class UserController {
             System.out.println("emailAuthMap: " + emailAuthStringMap);
         } catch (Exception e) {
             e.printStackTrace();
+            return ResponseEntity.internalServerError().body(new AuthMailResponse(false, null));
         }
 
         return responseEntity;
     }
 
+    @Operation(summary = "이메일 인증 코드 확인 API", description = "이메일 인증 코드 검증을 위한 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "code check: success(true) or fail(false)"),
+            @ApiResponse(responseCode = "500", description = "internal server error"),
+    })
     @GetMapping("code/check")
     public ResponseEntity<CodeCheckResponse> authEmailCheck(@RequestParam("email") String email, @RequestParam("code") String authCode) {
         if (emailAuthStringMap.containsKey(email) && emailAuthStringMap.get(email).equals(authCode)) {
