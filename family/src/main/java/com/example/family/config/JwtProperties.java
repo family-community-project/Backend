ac6f97790d4dc0c357c6dc911fe82630ef019b83
gomdol <oioifor@naver.com>
2025-07-09 18:09:41 +0900

    feat: add JwtProperties

--- family/src/main/java/com/example/family/config/JwtProperties.java
+++ family/src/main/java/com/example/family/config/JwtProperties.java
@@ -1,14 +1,12 @@
 package com.example.family.config;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.Base64;
 
-@Getter
-@Setter
+@Data
 @Component
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
